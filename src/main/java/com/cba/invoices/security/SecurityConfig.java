d46aa5594b0f4d3fb622bd92f59bb6f820bd9d3e
CBA <asanbuga@gmail.com>
2025-04-22 23:57:11 +0300

    1- Basic Auth. kullanıcı ve rollerinii DB'den alması için değişiklik yapıldı.
    2- Projeyi test etmek isteyenler için DB scripti eklendi.

--- src/main/java/com/cba/invoices/security/SecurityConfig.java
+++ src/main/java/com/cba/invoices/security/SecurityConfig.java
@@ -6,13 +6,39 @@ import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+
+import javax.sql.DataSource;
 
 @Configuration
 public class SecurityConfig {
+
+    @Bean
+    public UserDetailsManager detailsManager (DataSource dataSource){
+        // Özelleştirilmiş bir tablo kullanmayacağım şuan için. Spring'in istediği mevcut yapıyı kullanacağım.
+        return new JdbcUserDetailsManager(dataSource);
+    }
+
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity security) throws Exception {
+        security.authorizeHttpRequests(config -> config
+                .requestMatchers(HttpMethod.POST, "api/customers").hasRole("MANAGER")
+                .requestMatchers(HttpMethod.POST, "api/invoices").hasAnyRole("MANAGER", "USER")
+                .requestMatchers(HttpMethod.GET, "api/invoices/**").hasAnyRole("MANAGER", "USER")
+
+        );
+        security.csrf(AbstractHttpConfigurer::disable);
+        security.httpBasic(Customizer.withDefaults());
+        return security.build();
+    }
+
+    /*
+    #
+    # Basic Auth için kullanıcı ve rolleri DB üzerinden çekeceğim için burayı kapattım.
+    #
 
     @Bean
     public InMemoryUserDetailsManager userDetailsManager() {
@@ -31,18 +57,5 @@ public class SecurityConfig {
 
         return new InMemoryUserDetailsManager(cba, hba);
 
-    }
-
-    @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity security) throws Exception {
-        security.authorizeHttpRequests(config -> config
-                .requestMatchers(HttpMethod.POST, "api/customers").hasRole("MANAGER")
-                .requestMatchers(HttpMethod.POST, "api/invoices").hasAnyRole("MANAGER", "USER")  // tek seviye için
-                .requestMatchers(HttpMethod.GET, "api/invoices/**").hasAnyRole("MANAGER", "USER") // alt path'ler için
-
-        );
-        security.csrf(AbstractHttpConfigurer::disable);
-        security.httpBasic(Customizer.withDefaults());
-        return security.build();
-    }
+    }*/
 }
